c2dc7e855ecfec9a8e2845220b0653a5114add8d
Yasin SARICA <yasin-sarica@outlook.com>
2023-03-11 02:07:44 +0300

    11032023-0207 updateresume

--- netcore-portfolio/Controllers/ResumeSettings.cs
+++ netcore-portfolio/Controllers/ResumeSettings.cs
@@ -8,18 +8,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using netcore_portfolio.Models;
+using System.IO;
 
 namespace netcore_portfolio.Controllers
 {
     [Authorize]
-    
+
     public class ResumeSettings : Controller
     {
         private readonly Context _context;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public ResumeSettings(Context context,SignInManager<ApplicationUser> signInManager,UserManager<ApplicationUser> userManager)
+        public ResumeSettings(Context context, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _context = context;
             _signInManager = signInManager;
@@ -30,16 +31,95 @@ namespace netcore_portfolio.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<JsonResult> UpdateResume(Resume model)
+        {
+            var resume = _context.Resume.FirstOrDefault(x => x.ResumeID == 1);
+            resume.Name = model.Name;
+            resume.Surname = model.Surname;
+            resume.TitleDescription = model.TitleDescription;
+            resume.Title = model.Title;
+            resume.Phone = model.Phone;
+            resume.Mail = model.Mail;
+            resume.Location = model.Location;
+            resume.ResumeAbout = model.ResumeAbout;
+
+            _context.SaveChanges();
+
+            var user = await _userManager.FindByEmailAsync(User.Identity.Name);
+            if (user != null)
+            {
+                user.Name = model.Name;
+                user.Surname = model.Surname;
+                await _userManager.UpdateAsync(user);
+            }
+
+            return Json(new { success = true });
+        }
         public IActionResult GetResume()
         {
             var resume = _context.Resume.FirstOrDefault(x => x.ResumeID == 1);
             return Json(resume);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> ResumeImageUpdate(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("Resim dosyası seçilmedi");
+
+            if (!IsImage(file))
+                return BadRequest("Yalnızca resim dosyaları yüklenebilir");
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "resume", fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var userImage = _context.Resume.FirstOrDefault(x => x.ResumeID == 1);
+            var oldImage = userImage.ResumeImage;
+            userImage.ResumeImage = "/images/resume/" + fileName;
+            _context.SaveChanges();
+            var user = await _userManager.FindByEmailAsync(User.Identity.Name);
+            if (user != null)
+            {
+                user.UserImage = "/images/resume/" + fileName;
+                await _userManager.UpdateAsync(user);
+            }
+            if (oldImage != "/images/default/no-profile-photo.webp")
+            {
+                if (!string.IsNullOrEmpty(oldImage))
+                {
+                    var oldFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", oldImage.TrimStart('/'));
+                    if (System.IO.File.Exists(oldFilePath))
+                        System.IO.File.Delete(oldFilePath);
+                }
+            }
+            return Json(new { success = true });
+        }
+
+        private bool IsImage(IFormFile file)
+        {
+            if (file.ContentType.ToLower() != "image/jpg" &&
+                file.ContentType.ToLower() != "image/jpeg" &&
+                file.ContentType.ToLower() != "image/pjpeg" &&
+                file.ContentType.ToLower() != "image/gif" &&
+                file.ContentType.ToLower() != "image/x-png" &&
+                file.ContentType.ToLower() != "image/png")
+            {
+                return false;
+            }
+
+            return true;
+        }
         public IActionResult GetEducation()
         {
-       
-            var edu1 = _context.Hobbies.Where(x => x.ResumeID == 2).ToList();
-            return Json(edu1);
+            var user = _userManager.FindByEmailAsync(User.Identity.Name);
+
+            return Json(user.Result);
         }
 
     }
---
+++ netcore-portfolio/wwwroot/js/ResumeSettings.js
@@ -0,0 +1,106 @@
+﻿
+    function getResumeData() {
+        $.ajax({
+            type: 'GET',
+            url: '/ResumeSettings/GetResume/',
+            success: function (resume) {
+                $('#name').val(resume.name);
+                $('#surname').val(resume.surname);
+                $('#titleDescription').val(resume.titleDescription);
+                $('#title').val(resume.title);
+                $('#phone').val(resume.phone);
+                $('#mail').val(resume.mail);
+                $('#location').val(resume.location);
+                $('#resumeAbout').val(resume.resumeAbout);
+                $('#resumeimage').attr('src', resume.resumeImage);
+            },
+            error: function () {
+                Swal.fire({
+                    icon: 'danger',
+                    title: 'Özgeçmiş Bilgi Düzenleme',
+                    text: 'Malesef Bir Hata Oluştu,verilere ulaşamıyoruz.'
+                });
+            }
+        });
+    }
+    $('#updateButton').click(function (e) {
+        e.preventDefault();
+        var model = {
+            name: $('#name').val(),
+            surname: $('#surname').val(),
+            titleDescription: $('#titleDescription').val(),
+            title: $('#title').val(),
+            phone: $('#phone').val(),
+            mail: $('#mail').val(),
+            location: $('#location').val(),
+            resumeAbout: $('#resumeAbout').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/ResumeSettings/UpdateResume',
+            data: model,          
+            success: function (response) {
+                if (response.success) {
+                    getResumeData();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Özgeçmiş Genel Bilgiler Başarıyla Güncellendi',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                } else {
+                    Swal.fire({
+                        icon: 'danger',
+                        title: 'Özgeçmiş Bilgi Düzenleme',
+                        text: 'Malesef Bir Hata Oluştu,verilere ulaşamıyoruz.'
+                    });
+                }
+            },
+            error: function () {
+                Swal.fire({
+                    icon: 'danger',
+                    title: 'Özgeçmiş Bilgi Düzenleme',
+                    text: 'Malesef Bir Hata Oluştu,verilere ulaşamıyoruz.'
+                });
+            }
+        });
+    });
+
+    $('#saveResumeImage').click(function (e) {
+        e.preventDefault();
+        var formData = new FormData();
+        formData.append('file', $('#resumeImage')[0].files[0]);
+
+        $.ajax({
+            url: '/ResumeSettings/ResumeImageUpdate',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                if (response.success) {
+                    getResumeData();
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Resim Yükleme',
+                        text: 'Resim başarıyla yüklendi'
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Resim Yükleme',
+                        text: 'Resim yüklenirken bir hata oluştu'
+                    });
+                }
+            },
+            error: function (xhr, status, error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Resim Yükleme',
+                    text: 'Resim yüklenirken bir hata oluştu'
+                });
+            }
+        });
+    });
