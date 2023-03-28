baaee66e281510a12856eb53b97c794f1a136d86
Yasin SARICA <yasin-sarica@outlook.com>
2023-03-28 17:18:51 +0300

    28032023-1718-update

---
+++ netcore-portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using netcore_portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcore_portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly Context _context;
+
+        public PortfolioController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PCategory()
+        {
+            return View();
+        }
+    }
+}
