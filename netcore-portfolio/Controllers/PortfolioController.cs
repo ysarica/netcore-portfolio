4a44c2f599a48594ddb77b699583599abfc6811e
Yasin SARICA <yasin-sarica@outlook.com>
2023-03-29 15:08:41 +0300

    29032023-1508-update

--- netcore-portfolio/Controllers/PortfolioController.cs
+++ netcore-portfolio/Controllers/PortfolioController.cs
@@ -23,5 +23,46 @@ namespace netcore_portfolio.Controllers
         {
             return View();
         }
+        public IActionResult GetPortfolioCategory()
+        {
+            var pCategory = _context.PortfolioCategory.ToList();
+
+            return Json(pCategory);
+        }
+        [HttpPost]
+        public IActionResult AddPortfolioCategory([FromForm] PortfolioCategory portfolioCategory)
+        {
+                _context.PortfolioCategory.Add(portfolioCategory);
+                _context.SaveChanges();
+                return Json(new { success = true });  
+        }
+        [HttpPost]
+        public IActionResult DeletePortfolioCategory(int id)
+        {
+            var portfolioCategory = _context.PortfolioCategory.FirstOrDefault(s => s.PCategoryID == id);
+            if (portfolioCategory != null)
+            {
+                _context.PortfolioCategory.Remove(portfolioCategory);
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+        [HttpGet]
+        public IActionResult GetPortfolioCategoryById(int id)
+        {
+            var portfolioCategory = _context.PortfolioCategory.FirstOrDefault(s => s.PCategoryID == id);
+            return Json(portfolioCategory);
+        }
+        [HttpPost]
+        public IActionResult UpdatePortfolioCategory([FromForm] PortfolioCategory portfolioCategory)
+        {
+            var oldPortfolioCategory = _context.PortfolioCategory.FirstOrDefault(x => x.PCategoryID == portfolioCategory.PCategoryID);
+
+            oldPortfolioCategory.PCategoryName = portfolioCategory.PCategoryName;
+            _context.SaveChanges();
+            return Json(new { success = true });
+
+        }
     }
 }
---
+++ netcore-portfolio/wwwroot/js/Portfolio.js
@@ -0,0 +1,171 @@
+﻿//PortfolioCategory Start
+function getPortfolioCategoryData() {
+    $.ajax({
+        type: "GET",
+        url: "/Portfolio/GetPortfolioCategory/",
+        success: function (data) {
+            var card = '';
+            $('#pCategory-cards').empty();
+            $.each(data, function (i, pCategory) {
+                card = '<div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">' +
+                    '<div class="card bg-light d-flex flex-fill">' +
+                    '<div class="card-header text-muted border-bottom-0 text-center">' + pCategory.pCategoryName + '</div>' +
+                   
+                    '<div class="card-footer">' +
+                    '<div class="text-right">' +
+                    '<div class="btn-group">' +
+                    '<button type="button" class="btn btn-sm btn-warning" onclick="editPCategory(' + pCategory.pCategoryID + ')">' +
+                    '<i class="fas fa-edit"></i> Düzenle' +
+                    '</button>' +
+                    '<button type="button" class="btn btn-sm btn-danger" onclick="deletePCategory(' + pCategory.pCategoryID + ')">' +
+                    '<i class="fas fa-trash"></i> Sil' +
+                    '</a>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>';
+                $('#pCategory-cards').append(card);
+            });
+        },
+        error: function () {
+            Swal.fire({
+                icon: 'danger',
+                title: 'Portfolyo Kategori',
+                text: 'Bir Hata Oluştu Verileri Çekemiyoruz !'
+            });
+        }
+    });
+}
+
+$('#addPortfolioCategoryButton').click(function (e) {
+    e.preventDefault();
+    var formData = new FormData();
+    formData.append('PCategoryName', $('#PCategoryName').val());
+
+    $.ajax({
+        type: "POST",
+        url: "/Portfolio/AddPortfolioCategory",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            $('#modal-addPortfolioCategory').modal('hide');
+            console.log(data);
+            getPortfolioCategoryData();
+            $('#PCategoryName').val('');
+            Swal.fire({
+                icon: 'success',
+                title: 'Portfolyo Kategori',
+                text: 'Kategori Başarıyla Eklendi'
+            });
+        },
+        error: function (xhr, status, error) {
+            Swal.fire({
+                icon: 'danger',
+                title: 'Portfolyo Kategori',
+                text: 'Bir Hata Oluştu !'
+            });
+        }
+    });
+});
+
+function deletePCategory(pCategoryID) {
+    Swal.fire({
+        title: 'Emin misiniz?',
+        text: "Bu işlemi geri alamazsınız!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Evet, sil!',
+        cancelButtonText: 'İptal'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: '/Portfolio/DeletePortfolioCategory',
+                data: { 'id': pCategoryID },
+                type: 'POST',
+                success: function (data) {
+                    if (data.success) {
+                        Swal.fire(
+                            'Silindi!',
+                            'Kategori başarıyla silindi.',
+                            'success'
+                        ).then(() => {
+                            getPortfolioCategoryData();
+                        });
+                    } else {
+                        Swal.fire(
+                            'Hata!',
+                            'Kategori silinirken bir hata oluştu. 1',
+                            'error'
+                        );
+                    }
+                },
+                error: function () {
+                    Swal.fire(
+                        'Hata!',
+                        'Kategori silinirken bir hata oluştu. 2',
+                        'error'
+                    );
+                }
+            });
+        }
+    });
+}
+
+function editPCategory(serviceID) {
+    $.ajax({
+        type: "GET",
+        url: "/Portfolio/GetPortfolioCategoryById/" + serviceID,
+        success: function (data) {
+            $('#PCategoryNameU').val(data.pCategoryName);
+            $('#PCategoryID').val(data.pCategoryID);
+
+            $('#modal-updatePortfolioCategory').modal('show');
+
+            $('#update-portfoliocategory-form').on('submit', function (e) {
+                e.preventDefault();
+                var formData = new FormData();
+
+                formData.append('PCategoryName', $('#PCategoryNameU').val());
+                formData.append('PCategoryID', $('#PCategoryID').val());
+
+                $.ajax({
+                    type: "POST",
+                    url: "/Portfolio/UpdatePortfolioCategory/",
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function () {
+                        $('#modal-updatePortfolioCategory').modal('hide');
+                        getPortfolioCategoryData();
+                        serviceID = null;
+                        formData = null;
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Portfolyo Kategori',
+                            text: 'Kategori Başarıyla Güncellendi'
+                        });
+                    },
+                    error: function () {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Portfolyo Kategori',
+                            text: 'Kategori güncelleme sırasında bir hata oluştu.'
+                        });
+                    }
+                });
+            });
+        },
+        error: function () {
+            Swal.fire({
+                icon: 'error',
+                title: 'Portfolyo Kategori',
+                text: 'Kategori verileri getirilirken bir hata oluştu.'
+            });
+        }
+    });
+}
+//PortfolioCategory Finish
